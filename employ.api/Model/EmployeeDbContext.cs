faf4c7fd66612804ba27aabfbec9aff2bda1e642
cjlicayan <cjlicayan@PHTGMLPT098>
2025-07-23 11:16:29 +0800

    Add Entity Framework Core support for SQL Server
    Introduce EmployeeDbContext for database interactions.
    Configure database connection string in appsettings.json.
    Update Program.cs to register necessary services for EF Core.

---
+++ employ.api/Model/EmployeeDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace employ.api.Model
+{
+    public class EmployeeDbContext:DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options)
+        {
+        }
+    }
+}
--- employ.api/Program.cs
+++ employ.api/Program.cs
@@ -1,6 +1,12 @@
+using employ.api.Model;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<EmployeeDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("testconnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
